6bdfcb314ca19924525dfb6769bace8b7d337219
Ryan Whitman <ryanawhitman@gmail.com>
2018-04-11 19:56:15 -0500

    Upgraded to v2.0.
    - Time now gets passed into constructor.
    - Can add multiple callbacks.

--- README.md
+++ README.md
@@ -1,28 +1,7 @@
-# jsMinTime
-jsMinTime is a JavaScript tool that grants the developer the ability to establish a minimum amount of time that is to elapse prior to executing a function.
+# Callback_Wait
 
-## Usage
-**Instantiate Timer:** `var myVar = new jsMinTime();`
+Callback_Wait is a JavaScript tool that allows a developer to establish a minimum amount of time that must elapse prior to executing one or more callbacks. This tool is especially useful for delaying a quick AJAX response that may otherwise cause a spinner to flash on/off, or something to that nature. This software is designed to be >=IE11 compatible.
 
-**Execute Function:** `myVar.exe(function, milliseconds, param1, param2, ...);`
+## Current Version
 
-## Example
-```javascript
-// Display the spinner and start jsMinTime
-jQuery('#my-spinner').show();
-var spinner = new jsMinTime();
-
-// Make AJAX call
-jQuery.ajax({
-    url: '/my-url',
-    success: function(res) {
-
-        // Do stuff with the response
-        do_stuff(res);
-        
-        // Hide the spinner - We want the spinner to display for a minimum of a half second. If the AJAX call was quick and took .25 secs, the spinner will be hidden after an additional .25 secs. If the AJAX call took a while and responded after 2 secs, the spinner will be hidden immediately. 
-        spinner.exe(function() {
-			jQuery('#my-spinner').hide();
-		}, 500);
-	});
-```
+2.0.0
---
+++ src/callback_wait.js
@@ -0,0 +1,60 @@
+/**
+ * Callback_Wait is a JavaScript tool that allows a developer to establish a minimum amount of time that must elapse prior to executing one or more callbacks. This tool is especially useful for delaying a quick AJAX response that may otherwise cause a spinner to flash on/off, or something to that nature. This software is designed to be >=IE11 compatible.
+ *
+ * @package Callback_Wait
+ * @copyright Copyright (c) 2018 Ryan Whitman (https://ryanwhitman.com)
+ * @license https://opensource.org/licenses/MIT MIT
+ * @version 2.0.0
+ * @see https://github.com/RyanWhitman/callback_wait
+ */
+var Callback_Wait = function(millisecs_to_wait) {
+
+	// Store the start time.
+	var start_time = new Date().getTime();
+
+	// Default the millisecs.
+	if (typeof millisecs_to_wait === 'undefined')
+		millisecs_to_wait = 250;
+
+	// Create an array to hold the callbacks.
+	var callbacks = [];
+
+	/**
+	 * Add a callback.
+	 *
+	 * @param {Function}   func     The function to execute.
+	 * @param {mixed}      ...args  Any number of arguments to pass to the callback function.
+	 */
+	this.add_callback = function(func) {
+		callbacks.push({
+			func: func,
+			args: Array.prototype.slice.call(arguments, 1)
+		});
+	}
+
+	/**
+	 * Execute the callbacks. The callbacks will get executed after the minimum wait time has elapsed. Optionally pass in a callback. See add_callback() for argument definition.
+	 */
+	this.exe = function(func) {
+
+		// If a callback was passed in, add it.
+		if (typeof func === 'function') {
+			var args = Array.prototype.slice.call(arguments, 1);
+			args.unshift(func);
+			this.add_callback.apply(null, args);
+		}
+
+		// Grab the current time.
+		var current_time = new Date().getTime();
+
+		// Determine the amount of time that is remaining.
+		var time_remaining = Math.max((start_time + millisecs_to_wait) - current_time, 0);
+
+		// Set a timeout that will loop through and execute the callbacks at the end of the remaining time. If the wait time has already elapsed, "time_remaining" will be 0 and the functionality will run immediately.
+		setTimeout(function() {
+			callbacks.forEach(function(obj) {
+				obj.func.apply(null, obj.args);
+			});
+		}, time_remaining);
+	};
+};
